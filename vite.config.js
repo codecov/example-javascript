12b63e83e31eaa8f3cb5bf7dd7083c7f5fcfbe01
Tom Hu <thomas.hu@sentry.io>
2024-04-19 16:23:22 -0700

    feat: add Codecov bundler plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
+  plugins: [
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "example-javascript",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   test: {
     coverage: {
       provider: 'v8',
